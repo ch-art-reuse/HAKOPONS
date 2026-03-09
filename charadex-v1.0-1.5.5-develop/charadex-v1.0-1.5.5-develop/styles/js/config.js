084135f7a428fbbd7e3c548f33b1e80917b36c60
Charter <ch.art.reuse.uwu@gmail.com>
2026-03-09 16:46:07 +0800

    Update config.js

--- charadex-v1.0-1.5.5-develop/charadex-v1.0-1.5.5-develop/styles/js/config.js
+++ charadex-v1.0-1.5.5-develop/charadex-v1.0-1.5.5-develop/styles/js/config.js
@@ -26,7 +26,7 @@ charadex.site = {
 /* ==================================================================== */
 charadex.sheet = {
 
-  id: "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak",
+  id: "1rwSinnDMOGGFfl_dafSuP-tJx3T5Wj6RD-qN7cKArXo",
 
   pages: {
     masterlist:    "masterlist",
